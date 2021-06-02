89b1a1472325f015b05c04b2ef645019577797e3
ferranbecker <84651223+ferranbecker@users.noreply.github.com>
2021-06-01 19:57:52 -0700

    added form and search function

--- src/app.js
+++ src/app.js
@@ -42,9 +42,19 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
+  let apiKey = "cc1073ce88d46b1f446d8312dec62e79";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
-let apiKey = "cc1073ce88d46b1f446d8312dec62e79";
-let city = "Seattle";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+search("Seattle");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
