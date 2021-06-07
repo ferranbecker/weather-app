088f66307944686d69066d7fb21860c1a37acf4d
ferranbecker <84651223+ferranbecker@users.noreply.github.com>
2021-06-06 19:46:13 -0700

    API updates (day, rounding temps, changed index parameters for number of days displayed

--- src/app.js
+++ src/app.js
@@ -21,24 +21,39 @@ function formatDate(timestamp) {
 
   return `${day} ${hours}:${minutes}`;
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Mon", "Tues", "Wed", "Thu"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
             <div class="weather-forecast-date">
-            ${day}
+            ${formatDay(forecastDay.dt)}
             </div>
-            <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="" 
+            <img src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png" alt="" 
             width="42">
             <div class="weather-forecast-temperature">
-              <span class="weather-forecast-temperature-max">56°</span> <span class="weather-forecast-temperature-min">43°</span></div>
+              <span class="weather-forecast-temperature-max">${Math.round(
+                forecastDay.temp.max
+              )}°</span> <span class="weather-forecast-temperature-min">${Math.round(
+          forecastDay.temp.min
+        )}°</span></div>
 
         </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
