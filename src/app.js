35e33cd0885ae3e1c7aeeb4ab00d09ad140eafcb
ferranbecker <84651223+ferranbecker@users.noreply.github.com>
2021-06-06 17:04:49 -0700

    Injected HTML into JS for forecast, creating new functions

--- src/app.js
+++ src/app.js
@@ -21,6 +21,28 @@ function formatDate(timestamp) {
 
   return `${day} ${hours}:${minutes}`;
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Mon", "Tues", "Wed", "Thu"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+            <div class="weather-forecast-date">
+            ${day}
+            </div>
+            <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="" 
+            width="42">
+            <div class="weather-forecast-temperature">
+              <span class="weather-forecast-temperature-max">56°</span> <span class="weather-forecast-temperature-min">43°</span></div>
+
+        </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -84,3 +106,5 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 search("Seattle");
+
+displayForecast();
